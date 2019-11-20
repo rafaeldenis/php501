1e8ff9cedcdbbe923fc43dc3403a026b3c540b55
rafaeldenis <rafagdf85@gmail.com>
2019-11-19 22:58:42 -0200

    AULA6

--- aula6/pdo/conexao.php
+++ aula6/pdo/conexao.php
@@ -1,25 +1,47 @@
-<?php 
+<?php
 
-$servidor = 'mysql:host=localhost;dbname=blog';
-$usuario = 'developer';
-$senha = '4linux';
+class ConectDatabase
+{
+    public function __construct($servidor, $usuario, $senha)
+    {
+        // $servidor = 'mysql:host=localhost;dbname=blog';
+        // $usuario = 'developer';
+        // $senha = '4linux';
 
-try{
-     $conexao = new PDO($servidor,$usuario,$senha);
-     echo '<pre>';
-     echo "CONEXAO SUCESSO </br>";
+        $this->servidor = $servidor;
+        $this->usuario = $usuario;
+        $this->senha = $senha;
+        try {
+            $conexao = new PDO($this->servidor, $this->usuario, $this->senha);
+            $this->conexao = $conexao;
+            return $this->conexao;
+        } catch (PDOException $e) {
+            throw new Exception("Não foi possivel realizar a conexão", 500);
 
-     $res = $conexao->query('SELECT * FROM usuarios');
-
-     //var_dump($conexao);
-     foreach($res as $linha){
-         
-         print_r($linha);
-
-     }
-     $conexao = null;
-}catch(PDOException $e){
-    echo "ERRO {$e->getMessage()}</br>";
+        }
+    }
+    public function select($table)
+    {
+        return $this->conexao->query("SELECT * FROM {$table}");
+    }
 }
 
-?>
+$obj = new ConectDatabase('mysql:host=localhost;dbname=blog', 'developer', '4linux');
+
+foreach ($obj->select('usuarios') as $usuario) {
+    print_r($usuario);
+}
+// try {
+//     $conexao = new PDO($servidor, $usuario, $senha);
+//     echo '<pre>';
+//     echo "Conexão efetuada com sucesso</br>";
+
+//     $res = $conexao->query('SELECT * FROM usuarios');
+//     foreach ($res as $linha) {
+//         print_r($linha);
+//     }
+// } catch (PDOException $e) {
+//     echo "Erro {$e->getMessage()}</br>";
+// } finally {
+//     die();
+// }
